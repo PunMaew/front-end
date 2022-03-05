7f46275133d40d449c042fda7e023ee70afe79ef
warisara <warisara.khrua@mail.kmutt.ac.th>
2022-03-05 23:42:02 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>hello</h1>
+    <h2>hello</h2>
+    <h3>hello</h3>
+    <h4>hello</h4>
+    <h5>hello</h5>
+    <h6>hello</h6>
+    <p>hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+<style lang="scss" scoped>
+h1 {
+  color: $salmon;
+}
+</style>
