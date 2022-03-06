48aa8523fe9532c573d93cbb79cc66860776afe5
warisara <warisara.khrua@mail.kmutt.ac.th>
2022-03-06 17:27:14 +0700

    first commit dev branch

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>
-    <h1>hello</h1>
-    <h2>hello</h2>
-    <h3>hello</h3>
-    <h4>hello</h4>
-    <h5>hello</h5>
-    <h6>hello</h6>
-    <p>hello</p>
-  </div>
+  <!-- <v-app> -->
+  <!-- <div>
+      <h1>hello</h1>
+      <h2>hello</h2>
+      <h3>hello</h3>
+      <h4>hello</h4>
+      <h5>hello</h5>
+      <h6>hello</h6>
+      <p>hello</p>
+    </div> -->
+  <section>
+    <header>
+      <v-container>
+        <v-row justify="center">
+          <v-col sm="7">
+            <nuxt-link to="/">
+              <!-- <img id="logo" src="" alt="" /> -->
+              <p>PUNMAEW</p>
+            </nuxt-link>
+          </v-col>
+          <v-col sm="5">
+            <div class="text-right d-flex justify-end">
+              <ul id="menu" class="d-none d-md-flex">
+                <li>
+                  <n-link to="/">บทความ</n-link>
+                </li>
+                <li>
+                  <n-link to="/"> หาบ้านให้แมว </n-link>
+                </li>
+                <li>
+                  <n-link to="/">เข้าสู่ระบบ </n-link>
+                </li>
+                <li>
+                  <button>สมัครสมาชิก</button>
+                </li>
+              </ul>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </header>
+  </section>
+  <!-- </v-app> -->
 </template>
 
 <script>
@@ -16,7 +50,15 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-h1 {
-  color: $salmon;
+ul#menu {
+  padding: 0;
+  display: flex;
+  li {
+    list-style: none;
+    align-self: center;
+    & + li {
+      margin-left: 40px;
+    }
+  }
 }
 </style>
